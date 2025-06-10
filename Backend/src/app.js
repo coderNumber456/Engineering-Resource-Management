eafaa7d8881f1ee7330b40ecdeee40c0a351a233
coderNumber456 <suf28031999@gmail.com>
2025-06-10 20:48:28 +0530

    cookie-session

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cookie from "cookie-session"
 
 const app = express()
 
