8c8bc1d2201fe4b2e6bb8d1a7eab3fcc8d25b378
coderNumber456 <suf28031999@gmail.com>
2025-06-10 20:33:33 +0530

    cookies

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,8 +16,18 @@ app.use(cors({
 
 app.use(cookieParser())
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(cookie({
+    httponly: true,
+    secure: true,
+    sameSite: "none"
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+
+
+
 
 // routes
 
